fa9d67cde5120cce80ffdb469dc89ec5d0166182
kmsec-uk <kmsec-uk@dev.null>
2025-12-16 09:34:43 +0000

    implement simple 10s timeout before hitting registry

--- cmd/printer/main.go
+++ cmd/printer/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"log"
+	"sync"
 	"time"
 
 	"github.com/kmsec-uk/npm-follower/couch"
@@ -10,10 +11,12 @@ import (
 
 func main() {
 	ctx := context.Background()
-	log.Println("helllo from the printer")
+	log.Println("hello from the printer")
 	// create the follower
 	f := couch.NewFollower().WithPollingInterval(5 * time.Second)
 	// connect and start receiving changes from the channel
+
+	var wg sync.WaitGroup
 	for event := range f.Connect(ctx) {
 		// skip error cases
 		if err := event.Error; err != nil {
@@ -26,19 +29,34 @@ func main() {
 			log.Printf("%s: deleted\n", event.Change.ID)
 			continue
 		}
+		// start goroutine
+		wg.Go(func() {
+			select {
 
-		// get full packument details
-		p, err := f.GetPackument(ctx, &event.Change)
-		if err != nil {
-			log.Printf("%s: error getting packument: %v\n", event.Change.ID, err)
-			continue
-		}
+			case <-time.After(10 * time.Second):
+				// wait 10 seconds before getting packument
+			case <-ctx.Done():
+				return
+			}
+			// get full packument details
+			p, err := f.GetPackument(ctx, &event.Change)
+			if err != nil {
+				log.Printf("%s: error getting packument: %v\n", event.Change.ID, err)
+				return
+			}
 
-		// check that the Packument _rev property is aligned with the _changes feed _rev
-		if !event.Change.HasRevision(p.Rev) {
-			log.Printf("%s: Packument revision (_rev property) %s not in _changes feed. CouchDB says %s", event.Change.ID, p.Rev, event.Change.Changes[0].Rev)
-		}
-		latest := p.Latest()
-		log.Printf("%s: updated - latest version %s was published by npm user %s (%s)", event.Change.ID, latest.Version, latest.NpmUser.Name, latest.NpmUser.Email)
+			// check that the Packument _rev property is aligned with the _changes feed _rev
+			if !event.Change.HasRevision(p.Rev) {
+				log.Printf("%s: Packument revision (_rev property) %s not in _changes feed. CouchDB says %s", event.Change.ID, p.Rev, event.Change.Changes[0].Rev)
+			}
+			// do something with the Packument here -- this example gets the latest
+			// version (Latest() convenience utility and logs version manifest metadata)
+			latest := p.Latest()
+			log.Printf("%s: updated - latest version %s was published by npm user %s (%s)", event.Change.ID, latest.Version, latest.NpmUser.Name, latest.NpmUser.Email)
+		})
+
 	}
+	// Wait for all the 10-second timers to finish before quitting completely
+	log.Println("shutting down, waiting for pending workers...")
+	wg.Wait()
 }
